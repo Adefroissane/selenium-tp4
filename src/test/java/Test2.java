b5afab7e16b8e6368bcc8529391374762ad50c35
Adefroissane <adefroissane@hotmail.fr>
2018-07-10 16:51:34 +0200

    files all

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test2 {
+
+    @Test
+    public void t1(){
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://www.google.com");
+        driver.close();
+    }
 }
